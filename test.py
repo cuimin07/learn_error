042fc9531b2e9fccaab1053b4acccf57028d7079
42589 <425891206@qq.com>
2020-08-14 17:17:23 +0800

    修改test.py

--- test.py
+++ test.py
@@ -3,3 +3,19 @@
 # @File    : test.py
 # @Date    : 2020-08-14
 # @Author  : CuiMin
+
+while True:
+    try:
+        x = int(input("input x number:"))
+        break
+    except ValueError:
+        print("no number!")
+
+
+# 同时处理多异常
+try:
+    x = abs(1+1)
+except (RuntimeError, TypeError, NameError):
+    pass
+
+
